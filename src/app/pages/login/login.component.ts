a6fcfccecf0b53baf463c3fb43458a211604896a
viniciuspani <viniciuspani@hotmail.com>
2024-10-29 23:54:09 -0300

    FEAT - Componente Login criado.

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup; // ! operador de asserção não nulo do TypeScript. Diz que a variavel nao eh nula.
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AutenticacaoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null],
+      password: [null]
+    })
+  }
+
+  login() {
+    // const email = this.loginForm.get('email')?.value;
+    // const password = this.loginForm.get('password')?.value;
+    const email = this.loginForm.value.email;
+    const senha = this.loginForm.value.senha;
+
+    this.authService.autenticar(email, senha).subscribe({
+      next: (value) => {
+        console.log('Login realizado com sucesso.', value),
+        this.router.navigateByUrl('/');
+      },
+      error: (err) => console.log('Erro no login', err)
+    });
+  }
+}
