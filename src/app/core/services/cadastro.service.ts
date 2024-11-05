fc47117d576fc82a83fec023162125e4502647b4
viniciuspani <viniciuspani@hotmail.com>
2024-11-05 16:41:44 -0300

    FEAT - Adicao de funcionalidade de buscar cadastro e editar cadastro no CadastroService.

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
@@ -16,4 +16,15 @@ export class CadastroService {
   cadastrar(pessoaUsuaria: PessoaUsuaria): Observable<PessoaUsuaria> {
     return this.http.post<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro`, pessoaUsuaria);
   }
+
+  buscarCadastro(token: string): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders( { "Authorization ": `Bearer ${token}`});
+    return this.http.post<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro/perfil`, {headers});
+  }
+
+  editarCadastro(pessoaUsuaria: PessoaUsuaria, token: string): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders( { "Authorization ": `Bearer ${token}`});
+    return this.http.patch<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro/perfil`, pessoaUsuaria, {headers});
+  }
+
 }
